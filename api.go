badd439ea1f083f5383e740a771f5522e2045a8a
Charlie Revett <charlierevett@gmail.com>
2017-06-29 22:14:22 +0100

    Recursive Fetch (#3)

--- api.go
+++ api.go
@@ -2,6 +2,7 @@ package ytcomments
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/revett/go-yt-comments/models/api"
@@ -27,15 +28,36 @@ func NewAPI(key string) API {
 	}
 }
 
-// FetchComments receives a YouTube Video ID as an arguement, and returns a
-// slice of Comment structs.
-func (a API) FetchComments(videoID string) (*api.CommentThreadList, error) {
-	client := http.Client{}
-
-	request, err := a.formRequest(videoID)
+// FetchComments receives a YouTube Video ID and a MaxComments integer as
+// arguments. The Video ID is used to know what video to fetch comments for.
+// The MaxComments integer is used to know how many comments to fetch. The
+// function returns an array of CommentThreadList YouTube structs.
+func (a API) FetchComments(videoID string, maxComments int) ([]api.CommentThreadList, error) {
+	request, err := a.formRequest(videoID, "")
 	if err != nil {
 		return nil, err
 	}
+
+	var commentThreadLists []api.CommentThreadList
+
+	return a.fetch(request, maxComments, commentThreadLists)
+}
+
+func (a API) countComments(commmentThreadLists []api.CommentThreadList) int {
+	count := 0
+
+	for _, commmentThreadList := range commmentThreadLists {
+		for _, commentThread := range commmentThreadList.Items {
+			count++
+			count += len(commentThread.Replies.Comments)
+		}
+	}
+
+	return count
+}
+
+func (a API) fetch(request *http.Request, maxComments int, commmentThreadLists []api.CommentThreadList) ([]api.CommentThreadList, error) {
+	client := http.Client{}
 
 	response, err := client.Do(request)
 	if err != nil {
@@ -44,6 +66,13 @@ func (a API) FetchComments(videoID string) (*api.CommentThreadList, error) {
 
 	defer response.Body.Close()
 
+	if response.StatusCode > 200 {
+		return nil, fmt.Errorf(
+			"Non-200 status code returned from YouTube API, got: '%d'",
+			response.StatusCode,
+		)
+	}
+
 	var commentThreadList api.CommentThreadList
 
 	err = json.NewDecoder(response.Body).Decode(&commentThreadList)
@@ -51,10 +80,34 @@ func (a API) FetchComments(videoID string) (*api.CommentThreadList, error) {
 		return nil, err
 	}
 
-	return &commentThreadList, nil
+	commmentThreadLists = append(commmentThreadLists, commentThreadList)
+
+	if len(commentThreadList.Items) == 0 {
+		return commmentThreadLists, nil
+	}
+
+	if commentThreadList.NextPageToken == "" {
+		return commmentThreadLists, nil
+	}
+
+	commentCount := a.countComments(commmentThreadLists)
+
+	if commentCount < maxComments {
+		nextRequest, err := a.formRequest(
+			commentThreadList.Items[0].Snippet.VideoID,
+			commentThreadList.NextPageToken,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		return a.fetch(nextRequest, maxComments, commmentThreadLists)
+	}
+
+	return commmentThreadLists, nil
 }
 
-func (a API) formRequest(videoID string) (*http.Request, error) {
+func (a API) formRequest(videoID string, nextPageToken string) (*http.Request, error) {
 	request, err := http.NewRequest("GET", baseURI, nil)
 	if err != nil {
 		return nil, err
@@ -67,6 +120,10 @@ func (a API) formRequest(videoID string) (*http.Request, error) {
 	queryParams.Add("order", "time")
 	queryParams.Add("part", "snippet,replies")
 	queryParams.Add("videoId", videoID)
+
+	if nextPageToken != "" {
+		queryParams.Add("pageToken", nextPageToken)
+	}
 
 	request.URL.RawQuery = queryParams.Encode()
 
