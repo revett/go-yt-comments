da81bf30f9cd0b0c93a74d596a8f37d61a19b978
revett <2796074+revett@users.noreply.github.com>
2020-01-29 18:50:14 +0000

    Delete old code

--- ytc/models/api/comment_snippet_author_channel_id.go
+++
@@ -1,9 +0,0 @@
-package api
-
-type (
-	// CommentSnippetAuthorChannelID is a struct representation of the YouTube
-	// model.
-	CommentSnippetAuthorChannelID struct {
-		Value string `json:"value"`
-	}
-)
--- ytc/models/api/comment_thread_replies.go
+++
@@ -1,8 +0,0 @@
-package api
-
-type (
-	// CommentThreadReplies is a struct representation of the YouTube model.
-	CommentThreadReplies struct {
-		Comments []Comment `json:"comments"`
-	}
-)
